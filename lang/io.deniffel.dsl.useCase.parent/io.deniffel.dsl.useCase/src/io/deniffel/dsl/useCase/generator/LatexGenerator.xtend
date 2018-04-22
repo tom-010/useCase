0b800ed9d5a3570e2f725b572650b39610482ef4
Thomas Deniffel <tdeniffel@googlemail.com>
2018-04-22 10:03:43 +0200

    Static PDF finished, Some parts of PDF are dynamic

--- lang/io.deniffel.dsl.useCase.parent/io.deniffel.dsl.useCase/src/io/deniffel/dsl/useCase/generator/LatexGenerator.xtend
+++ lang/io.deniffel.dsl.useCase.parent/io.deniffel.dsl.useCase/src/io/deniffel/dsl/useCase/generator/LatexGenerator.xtend
@@ -11,12 +11,19 @@ import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.AbstractGenerator
 import org.eclipse.xtext.generator.IFileSystemAccess2
 import org.eclipse.xtext.generator.IGeneratorContext
+import io.deniffel.dsl.useCase.useCase.AllowedUserGroup
+import io.deniffel.dsl.useCase.useCase.AllowedUserGroups
+import io.deniffel.dsl.useCase.useCase.OptionalInputs
+import io.deniffel.dsl.useCase.useCase.Inputs
+import io.deniffel.dsl.useCase.useCase.Outputs
+import io.deniffel.dsl.useCase.useCase.PreConditions
+import io.deniffel.dsl.useCase.useCase.Steps
+import io.deniffel.dsl.useCase.useCase.Notes
+import io.deniffel.dsl.useCase.useCase.UsedTypes
 
 class LatexGenerator extends AbstractGenerator {
 	
 	ClassNamingStrategy classNamingStrategy = new ClassNamingStrategy();
-	ClassMemberNamingStrategy variableNaming = new ClassMemberNamingStrategy();
-	ClassMemberNamingStrategy methodNaming = new ClassMemberNamingStrategy();
 	ClassMemberNamingStrategy packageNaming = new ClassMemberNamingStrategy();
 	 
 	override void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context) {
@@ -26,25 +33,170 @@ class LatexGenerator extends AbstractGenerator {
 	}
 	
 	def create(UseCase usecase, Model model, IFileSystemAccess2 fsa) {
-		usecase.name = classNamingStrategy.convert(usecase.name);
-		val dirPath = model.company.replace(".", "/") + "/" + model.package.path + "/"
+		val dirPath = "/main/resources/documents/" + model.company.replace(".", "/") + "/" + model.package.path + "/"
 		fsa.generateFile(
-			"/main/resources/documents/"+ dirPath + usecase.name + ".tex", 
-			usecase.createLatexDocument(model.company, model.package, model.types.types, model.exceptions)
+			dirPath + usecase.camelCaseName + ".tex", 
+			model.createLatexDocument(usecase, dirPath)
 		);
 	}
 	
-	def createLatexDocument(UseCase usecase, String company, Package ucPackage, EList<Type> types, UsedExceptions exceptions) '''	
-	\documentclass[12pt]{article}
-	\begin{document}
-	Hello world!
-	$Hello world!$ %math mode 
-	\end{document}
+	def camelCaseName(UseCase uc) {
+		return classNamingStrategy.convert(uc.name)
+	}
+	
+	def createLatexDocument(Model model,UseCase usecase, String dirPath) '''	
+		\documentclass[a4paper,10pt]{scrartcl}
+		\usepackage{graphicx,wrapfig,lipsum}
+		
+		\usepackage[utf8]{inputenc}
+		\usepackage[T1]{fontenc}
+		\usepackage[german]{babel}
+		
+		\begin{document}
+		
+		\section{«usecase.name»}
+		
+		«usecase.diagram(dirPath)»
+		«model.overviewTable»
+		
+		\subsection{Erlaubt für}
+		«usecase.allowedUserGroups.get(0).subSection»
+		
+		\subsection{Zutaten}
+		«usecase.inputs.get(0).subSection(usecase.optionalInputs.get(0))»
+		
+		\subsection{Ergebins}
+		«usecase.outputs.get(0).subSection»
+		
+		\subsection{Vorbedingungen}
+		«usecase.preconditions.subSection»
+		
+		\subsection{Schritte}
+		«usecase.steps.get(0).subSection»
+		
+		\subsection{Anmerkungen}
+		«usecase.notes.get(0).notesSubSection»
+		
+		\subsection{Glossar}
+		\subsubsection{Genutze Bausteine}
+		«model.types.subSection»
+		
+		
+		\subsubsection{Genutzte Fehler}
+		«model.exceptions.subSection»
+		
+		\end{document}
 	'''
+	
+	def diagram(UseCase uc, String dirPath) '''
+		\begin{wrapfigure}{r}{5cm}
+		\includegraphics[width=5cm]{../../../../../../..«pathToDiagramPath(dirPath)»«uc.camelCaseName».png}
+		\end{wrapfigure} 
+	'''
+	
+	def overviewTable(Model model)'''
+		\begin{tabular}{ l l }
+		 Firma: & «model.company» \\ 
+		 Kathegorie: & «model.getPackage().compile» \\      
+		\end{tabular}
+	'''
+	
+	def subSection(AllowedUserGroups groups)'''
+		\begin{itemize}
+		  «FOR group : groups.groups»
+		  	\item «group.name»
+		  «ENDFOR»
+		\end{itemize}
+	'''
+	
+	def subSection(Inputs inputs, OptionalInputs optionals)'''
+		\subsubsection{Benötigte Zutaten}
+		\begin{itemize}
+		  \item Login als Text (z.B. tom)  
+		  \item Passwort als Text (z.B. beispiel\_passwort)
+		  \item optional Vorname als Text (z.B. Thomas)
+		  \item Nachname als Text (z.B. Deniffel)
+		  \item Alter als Zahl (z.B. 18) 
+		\end{itemize}
+		
+		\subsubsection{Optionale Zutaten}
+		\begin{itemize}
+		  \item Adresse als Text (z.B. Haldenweg 7a)
+		  \item Geschlecht als Text (z.B. m)
+		\end{itemize}
+	'''
+	
+	def subSection(Outputs outputs)'''
+		\begin{itemize}
+		  \item Erstellter Benutzer als Benutzer (z.B. Benutzer mit Login 'tom')
+		\end{itemize}
+	'''
+	
+	def subSection(PreConditions precoditions)'''
+		\begin{itemize}
+		  \item Angegebner Login muss im System einzigartig sein
+		  \item Nur eine weitere Vorbedingung
+		  \item Alter muss über 18 sein
+		\end{itemize}
+	'''
+	
+	def subSection(Steps steps)'''
+		\begin{enumerate}
+		  \item Falls Benutzer unter 18:
+		  \begin{enumerate}
+		   \item Melde Fehler UserToJungForTheSystem
+		  \end{enumerate}
+		  \item Suche Benutzer mit dem angegebenen Login
+		  \item Falls der Benutzer gefunden wurde:
+		  \begin{enumerate}
+		   \item Melde Fehler UserExistiertBereits 
+		  \end{enumerate}
+		  \item Solange nicht drei gute Geschenke gefunden wurden:
+		  \begin{enumerate}
+		  \item Suche nach Geschenk in der Datenbank
+		  \item Bewerte ob das Geschenk geeignet ist
+		  \item Ordne das Geschenk dem Benutzer zu
+		  \end{enumerate}
+		  \item Speichere den Benutzer in die Datenbank. Bei Fehler melde BenutzerKonnteNichtErstelltWerden
+		  \item Benachrichte alle, dass der Benutzer erstellt wurde
+		  \item Sende Willkommensnachricht an den Benutzer
+		\end{enumerate}
+	'''
+	
+	def notesSubSection(Notes steps)'''
+		Jeder User hat einen eigenen Login. Daher darf ein Login niemals doppelt vorkommen
+	'''
+	
+	def subSection(UsedTypes types)'''
+		\begin{itemize}
+			 \item File: Files sind Dateien und werden für unterschiedlichste Dinge eingesetzt
+			 \item Benutzer: Ein normaler Bentzer, der sich im System einloggen kann
+			 \item Text: Mehrere Zeichen ergeben ein Wort, mehrere Worte einen Text
+			 \item Zahl
+			 \item Zeichen: Die einzelnen Zeichen aus dem deutschen oder irgend einem anderen Alphabet
+		\end{itemize}
+	'''
+	
+	def subSection(UsedExceptions exceptions)'''
+		\begin{itemize}
+			 \item UserToJungForTheSystem
+			 \item UserExistiertBereits: Der angegebene Benutzer existiert bereits im System (der Login wurde bereits vergeben
+			 \item BenutzerKonnteNichtErstelltWerden: Das System konnte den Benutzer nicht erstellen
+			 \item IllegalArgumentException: Die Angegenen Paramter sind ungültig
+		\end{itemize}
+	'''
+	
+	def pathToDiagramPath(String pathToDir) {
+		return pathToDir.replace("documents", "diagrams");
+	}
 	
 	def path(Package ucPackage) {
 		return ucPackage.parts.join('/')[compile]
 	}
+		
+	def compile(Package ucPackage)'''
+		«ucPackage.parts.join(' $\\rightarrow$ ')[name]»
+	'''
 	
 	def compile(PackagePart part)'''
 		«packageNaming.convert(part.name)»'''
--- lang/io.deniffel.dsl.useCase.parent/io.deniffel.dsl.useCase/xtend-gen/io/deniffel/dsl/useCase/generator/LatexGenerator.java
+++ lang/io.deniffel.dsl.useCase.parent/io.deniffel.dsl.useCase/xtend-gen/io/deniffel/dsl/useCase/generator/LatexGenerator.java
@@ -3,11 +3,19 @@ package io.deniffel.dsl.useCase.generator;
 import com.google.common.collect.Iterables;
 import io.deniffel.dsl.useCase.generator.ClassMemberNamingStrategy;
 import io.deniffel.dsl.useCase.generator.ClassNamingStrategy;
+import io.deniffel.dsl.useCase.useCase.AllowedUserGroup;
+import io.deniffel.dsl.useCase.useCase.AllowedUserGroups;
+import io.deniffel.dsl.useCase.useCase.Inputs;
 import io.deniffel.dsl.useCase.useCase.Model;
+import io.deniffel.dsl.useCase.useCase.Notes;
+import io.deniffel.dsl.useCase.useCase.OptionalInputs;
+import io.deniffel.dsl.useCase.useCase.Outputs;
 import io.deniffel.dsl.useCase.useCase.PackagePart;
-import io.deniffel.dsl.useCase.useCase.Type;
+import io.deniffel.dsl.useCase.useCase.PreConditions;
+import io.deniffel.dsl.useCase.useCase.Steps;
 import io.deniffel.dsl.useCase.useCase.UseCase;
 import io.deniffel.dsl.useCase.useCase.UsedExceptions;
+import io.deniffel.dsl.useCase.useCase.UsedTypes;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -23,10 +31,6 @@ import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 public class LatexGenerator extends AbstractGenerator {
   private ClassNamingStrategy classNamingStrategy = new ClassNamingStrategy();
   
-  private ClassMemberNamingStrategy variableNaming = new ClassMemberNamingStrategy();
-  
-  private ClassMemberNamingStrategy methodNaming = new ClassMemberNamingStrategy();
-  
   private ClassMemberNamingStrategy packageNaming = new ClassMemberNamingStrategy();
   
   @Override
@@ -41,32 +45,351 @@ public class LatexGenerator extends AbstractGenerator {
   }
   
   public void create(final UseCase usecase, final Model model, final IFileSystemAccess2 fsa) {
-    usecase.setName(this.classNamingStrategy.convert(usecase.getName()));
     String _replace = model.getCompany().replace(".", "/");
-    String _plus = (_replace + "/");
+    String _plus = ("/main/resources/documents/" + _replace);
+    String _plus_1 = (_plus + "/");
     String _path = this.path(model.getPackage());
-    String _plus_1 = (_plus + _path);
-    final String dirPath = (_plus_1 + "/");
-    String _name = usecase.getName();
-    String _plus_2 = (("/main/resources/documents/" + dirPath) + _name);
-    String _plus_3 = (_plus_2 + ".tex");
-    fsa.generateFile(_plus_3, 
-      this.createLatexDocument(usecase, model.getCompany(), model.getPackage(), model.getTypes().getTypes(), model.getExceptions()));
+    String _plus_2 = (_plus_1 + _path);
+    final String dirPath = (_plus_2 + "/");
+    String _camelCaseName = this.camelCaseName(usecase);
+    String _plus_3 = (dirPath + _camelCaseName);
+    String _plus_4 = (_plus_3 + ".tex");
+    fsa.generateFile(_plus_4, 
+      this.createLatexDocument(model, usecase, dirPath));
   }
   
-  public CharSequence createLatexDocument(final UseCase usecase, final String company, final io.deniffel.dsl.useCase.useCase.Package ucPackage, final EList<Type> types, final UsedExceptions exceptions) {
+  public String camelCaseName(final UseCase uc) {
+    return this.classNamingStrategy.convert(uc.getName());
+  }
+  
+  public CharSequence createLatexDocument(final Model model, final UseCase usecase, final String dirPath) {
     StringConcatenation _builder = new StringConcatenation();
-    _builder.append("\\documentclass[12pt]{article}");
+    _builder.append("\\documentclass[a4paper,10pt]{scrartcl}");
+    _builder.newLine();
+    _builder.append("\\usepackage{graphicx,wrapfig,lipsum}");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\\usepackage[utf8]{inputenc}");
+    _builder.newLine();
+    _builder.append("\\usepackage[T1]{fontenc}");
+    _builder.newLine();
+    _builder.append("\\usepackage[german]{babel}");
+    _builder.newLine();
     _builder.newLine();
     _builder.append("\\begin{document}");
     _builder.newLine();
-    _builder.append("Hello world!");
     _builder.newLine();
-    _builder.append("$Hello world!$ %math mode ");
+    _builder.append("\\section{");
+    String _name = usecase.getName();
+    _builder.append(_name);
+    _builder.append("}");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    CharSequence _diagram = this.diagram(usecase, dirPath);
+    _builder.append(_diagram);
+    _builder.newLineIfNotEmpty();
+    CharSequence _overviewTable = this.overviewTable(model);
+    _builder.append(_overviewTable);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Erlaubt für}");
+    _builder.newLine();
+    CharSequence _subSection = this.subSection(usecase.getAllowedUserGroups().get(0));
+    _builder.append(_subSection);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Zutaten}");
+    _builder.newLine();
+    CharSequence _subSection_1 = this.subSection(usecase.getInputs().get(0), usecase.getOptionalInputs().get(0));
+    _builder.append(_subSection_1);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Ergebins}");
+    _builder.newLine();
+    CharSequence _subSection_2 = this.subSection(usecase.getOutputs().get(0));
+    _builder.append(_subSection_2);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Vorbedingungen}");
+    _builder.newLine();
+    CharSequence _subSection_3 = this.subSection(usecase.getPreconditions());
+    _builder.append(_subSection_3);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Schritte}");
+    _builder.newLine();
+    CharSequence _subSection_4 = this.subSection(usecase.getSteps().get(0));
+    _builder.append(_subSection_4);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Anmerkungen}");
+    _builder.newLine();
+    CharSequence _notesSubSection = this.notesSubSection(usecase.getNotes().get(0));
+    _builder.append(_notesSubSection);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("\\subsection{Glossar}");
+    _builder.newLine();
+    _builder.append("\\subsubsection{Genutze Bausteine}");
+    _builder.newLine();
+    CharSequence _subSection_5 = this.subSection(model.getTypes());
+    _builder.append(_subSection_5);
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\\subsubsection{Genutzte Fehler}");
+    _builder.newLine();
+    CharSequence _subSection_6 = this.subSection(model.getExceptions());
+    _builder.append(_subSection_6);
+    _builder.newLineIfNotEmpty();
     _builder.newLine();
     _builder.append("\\end{document}");
     _builder.newLine();
     return _builder;
+  }
+  
+  public CharSequence diagram(final UseCase uc, final String dirPath) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{wrapfigure}{r}{5cm}");
+    _builder.newLine();
+    _builder.append("\\includegraphics[width=5cm]{../../../../../../..");
+    String _pathToDiagramPath = this.pathToDiagramPath(dirPath);
+    _builder.append(_pathToDiagramPath);
+    String _camelCaseName = this.camelCaseName(uc);
+    _builder.append(_camelCaseName);
+    _builder.append(".png}");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\\end{wrapfigure} ");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence overviewTable(final Model model) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{tabular}{ l l }");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("Firma: & ");
+    String _company = model.getCompany();
+    _builder.append(_company, " ");
+    _builder.append(" \\\\ ");
+    _builder.newLineIfNotEmpty();
+    _builder.append(" ");
+    _builder.append("Kathegorie: & ");
+    CharSequence _compile = this.compile(model.getPackage());
+    _builder.append(_compile, " ");
+    _builder.append(" \\\\      ");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\\end{tabular}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final AllowedUserGroups groups) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    {
+      EList<AllowedUserGroup> _groups = groups.getGroups();
+      for(final AllowedUserGroup group : _groups) {
+        _builder.append("  ");
+        _builder.append("\\item ");
+        String _name = group.getName();
+        _builder.append(_name, "  ");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final Inputs inputs, final OptionalInputs optionals) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\subsubsection{Benötigte Zutaten}");
+    _builder.newLine();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Login als Text (z.B. tom)  ");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Passwort als Text (z.B. beispiel\\_passwort)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item optional Vorname als Text (z.B. Thomas)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Nachname als Text (z.B. Deniffel)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Alter als Zahl (z.B. 18) ");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\\subsubsection{Optionale Zutaten}");
+    _builder.newLine();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Adresse als Text (z.B. Haldenweg 7a)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Geschlecht als Text (z.B. m)");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final Outputs outputs) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Erstellter Benutzer als Benutzer (z.B. Benutzer mit Login \'tom\')");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final PreConditions precoditions) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Angegebner Login muss im System einzigartig sein");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Nur eine weitere Vorbedingung");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Alter muss über 18 sein");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final Steps steps) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{enumerate}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Falls Benutzer unter 18:");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\begin{enumerate}");
+    _builder.newLine();
+    _builder.append("   ");
+    _builder.append("\\item Melde Fehler UserToJungForTheSystem");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\end{enumerate}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Suche Benutzer mit dem angegebenen Login");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Falls der Benutzer gefunden wurde:");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\begin{enumerate}");
+    _builder.newLine();
+    _builder.append("   ");
+    _builder.append("\\item Melde Fehler UserExistiertBereits ");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\end{enumerate}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Solange nicht drei gute Geschenke gefunden wurden:");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\begin{enumerate}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Suche nach Geschenk in der Datenbank");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Bewerte ob das Geschenk geeignet ist");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Ordne das Geschenk dem Benutzer zu");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\end{enumerate}");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Speichere den Benutzer in die Datenbank. Bei Fehler melde BenutzerKonnteNichtErstelltWerden");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Benachrichte alle, dass der Benutzer erstellt wurde");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("\\item Sende Willkommensnachricht an den Benutzer");
+    _builder.newLine();
+    _builder.append("\\end{enumerate}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence notesSubSection(final Notes steps) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Jeder User hat einen eigenen Login. Daher darf ein Login niemals doppelt vorkommen");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final UsedTypes types) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item File: Files sind Dateien und werden für unterschiedlichste Dinge eingesetzt");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item Benutzer: Ein normaler Bentzer, der sich im System einloggen kann");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item Text: Mehrere Zeichen ergeben ein Wort, mehrere Worte einen Text");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item Zahl");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item Zeichen: Die einzelnen Zeichen aus dem deutschen oder irgend einem anderen Alphabet");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence subSection(final UsedExceptions exceptions) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("\\begin{itemize}");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item UserToJungForTheSystem");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item UserExistiertBereits: Der angegebene Benutzer existiert bereits im System (der Login wurde bereits vergeben");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item BenutzerKonnteNichtErstelltWerden: Das System konnte den Benutzer nicht erstellen");
+    _builder.newLine();
+    _builder.append("\t ");
+    _builder.append("\\item IllegalArgumentException: Die Angegenen Paramter sind ungültig");
+    _builder.newLine();
+    _builder.append("\\end{itemize}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public String pathToDiagramPath(final String pathToDir) {
+    return pathToDir.replace("documents", "diagrams");
   }
   
   public String path(final io.deniffel.dsl.useCase.useCase.Package ucPackage) {
@@ -74,6 +397,17 @@ public class LatexGenerator extends AbstractGenerator {
       return this.compile(it);
     };
     return IterableExtensions.<PackagePart>join(ucPackage.getParts(), "/", _function);
+  }
+  
+  public CharSequence compile(final io.deniffel.dsl.useCase.useCase.Package ucPackage) {
+    StringConcatenation _builder = new StringConcatenation();
+    final Function1<PackagePart, CharSequence> _function = (PackagePart it) -> {
+      return it.getName();
+    };
+    String _join = IterableExtensions.<PackagePart>join(ucPackage.getParts(), " $\\rightarrow$ ", _function);
+    _builder.append(_join);
+    _builder.newLineIfNotEmpty();
+    return _builder;
   }
   
   public CharSequence compile(final PackagePart part) {
